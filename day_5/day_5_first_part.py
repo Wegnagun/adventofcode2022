553113400d06a96b205a250c208f4cdc8719c867
Александр Фокин <wegnagun@bk.ru>
2022-12-10 18:17:40 +0300

    start day 5

--- day_5/day_5_first_part.py
+++ day_5/day_5_first_part.py
@@ -1,15 +1,20 @@
+# да емана, обрабатываем ввод, плюнул создал матрицу для контейнеров...
+containers = [['', 'D', ''], ['N', 'C', ''], ['Z', 'M', 'P'], [1, 2, 3]] # тестовые данные
+
+
 def load_data(file):  # Достаем вводные данные.
     with open(file) as file:
-        input_data = file.read().split()
-    return input_data
+        input_data = [line.rstrip() for line in file]
+        moves = input_data[5:]
+    return moves
 
 
 def main():
-    data = load_data('ttt.txt')
-    return data
+    moves = load_data('ttt.txt')
+    return moves
 
 
 if __name__ == '__main__':
     result = main()
-    print(result)
+    print(result, containers)
     # print(f'Количество входящих в друг друга =) пар: {result}')
