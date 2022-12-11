3746f451668b630cbb37602806d97d4d551bf4ea
Александр Фокин <wegnagun@bk.ru>
2022-12-11 16:58:09 +0300

    start day 7

--- day_7/day_6_first_part.py
+++ day_7/day_6_first_part.py
@@ -1,18 +1,12 @@
-def load_data(file):  # Достаем вводные данные.
+def load_data(file: str) -> str:  # Достаем вводные данные.
     with open(file) as file:
         return file.read()
 
 
 def main() -> int:
-    message = load_data('data.txt')
-    for index in range(len(message)):
-        if (
-            len(message[index: index + 4])
-            == len(set(message[index: index + 4]))
-        ):
-            return index + 4
+    pass
 
 
 if __name__ == '__main__':
     result = main()
-    print(f'Пакет начинается с символа под номером: {result}')
+    print(f'Надо обработать {result} символов а надо 3263')
